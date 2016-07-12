09eee5d515be8913b371a0407cdbffbfc0e083d3
Alessandro Giannini <alessandro@develer.com>
2016-07-12 12:04:15 +0200

    Add react-native-search-bar package to the repo.

--- app/js/pages/glossary.js
+++ app/js/pages/glossary.js
@@ -4,16 +4,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import SearchBar from 'react-native-search-bar';
+
 import * as style from '../style';
 
 
 export default class Glossary extends Component {
   render() {
-    const { container } = style.pages.glossary;
-    const { debug } = style.common;
+    const { container, searchView } = style.pages.glossary;
+    const { debug, debug1 } = style.common;
 
     return (
       <View style={[container, debug]}>
+        <View style={[searchView, debug1]}>
+          <SearchBar />
+        </View>
         <Text>Hello Glossary!</Text>
       </View>
       );
