b4410ec2cc54c9857ace2d0b496b137d6db1f6e6
Alessandro Giannini <alessandro@develer.com>
2016-07-19 10:12:30 +0200

    Filter glossary from search bar.

--- app/js/pages/glossary.js
+++ app/js/pages/glossary.js
@@ -13,20 +13,38 @@ import * as style from '../style';
 export default class Glossary extends Component {
   constructor(props: any) {
     super(props);
+    this._searchBar = null;
+    this.state = { filterArgs: null };
+  }
+
+  onChangeSearchText(text) {
+    this.setState({ filterArgs: text === '' ? null : [text] });
+  }
+
+  hideSearchBar() {
+    this._searchBar.blur();
   }
 
   render() {
     const { flexible } = style.common;
+    const filterStr = `label CONTAINS[cd] %@`;
 
     return (
       <View style={[flexible]}>
-        <SearchBar placeholder={'Cerca'} />
+        <SearchBar ref={(c) => this._searchBar = c}
+                   placeholder={'Cerca'}
+                   onChangeText={this.onChangeSearchText.bind(this)}
+                   onSearchButtonPress={this.hideSearchBar.bind(this)}
+                   onCancelButtonPress={this.hideSearchBar.bind(this)} />
         <TableView tableViewStyle={TableView.Consts.Style.Plain}
                    tableViewCellStyle={TableView.Consts.CellStyle.Subtitle}
                    style={flexible}
-                   // textColor={'#FF7700'}
                    detailFontSize={9}
-                   json="glossary">
+                   showIndexList={this.state.filterArgs === null}
+                   json="glossary"
+                   filter={filterStr}
+                   filterArgs={this.state.filterArgs}
+                   showEmptySections={false}>
         </TableView>
       </View>
       );
