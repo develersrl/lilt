b1946e1e428f23bc5e475caae69c0a992dd3f311
Alessandro Giannini <alessandro@develer.com>
2016-07-19 11:41:53 +0200

    Add cancel button to search bar.

--- app/js/pages/glossary.js
+++ app/js/pages/glossary.js
@@ -14,28 +14,35 @@ export default class Glossary extends Component {
   constructor(props: any) {
     super(props);
     this._searchBar = null;
-    this.state = { filterArgs: null };
+    this.state = { filterArgs: null, searchFocused: false };
   }
 
   onChangeSearchText(text) {
     this.setState({ filterArgs: text === '' ? null : [text] });
   }
 
-  hideSearchBar() {
-    this._searchBar.blur();
+  clearFilter() {
+    this.setState({ ...this.state, filterArgs: null });
+  }
+
+  setSearchFocus(b) {
+    this.setState({ ...this.state, searchFocused: b });
   }
 
   render() {
     const { flexible } = style.common;
-    const filterStr = `label CONTAINS[cd] %@`;
+    const filterStr = `label CONTAINS[d] %@`;
 
     return (
       <View style={[flexible]}>
         <SearchBar ref={(c) => this._searchBar = c}
                    placeholder={'Cerca'}
+                   showsCancelButton={this.state.searchFocused}
                    onChangeText={this.onChangeSearchText.bind(this)}
-                   onSearchButtonPress={this.hideSearchBar.bind(this)}
-                   onCancelButtonPress={this.hideSearchBar.bind(this)} />
+                   onSearchButtonPress={() => this._searchBar.blur()}
+                   onCancelButtonPress={this.clearFilter.bind(this)}
+                   onFocus={() => this.setSearchFocus(true)}
+                   onBlur={() => this.setSearchFocus(false)}/>
         <TableView tableViewStyle={TableView.Consts.Style.Plain}
                    tableViewCellStyle={TableView.Consts.CellStyle.Subtitle}
                    style={flexible}
