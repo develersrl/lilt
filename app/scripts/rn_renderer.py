9f7682dc6c22d0bb0dc65286c2a2078b75a80ce8
Alessandro Giannini <alessandro@develer.com>
2016-07-27 12:36:07 +0200

    Improve markdown renderer documentation.

--- app/scripts/rn_renderer.py
+++ app/scripts/rn_renderer.py
@@ -5,7 +5,17 @@ import mistune
 
 
 class Wrapper(object):
-    """Generic class wrapper."""
+    """Generic class wrapper.
+
+    Use case: you need to add the support for a new markdown feature but you
+    do not know which renderer method to override.
+    In this case you can build mistune renderer this way:
+
+        renderer = mistune.Markdown(renderer=Wrapper(RNRenderer))
+        renderer(<markdown-code-here>)
+
+    Called methods will be printed on stdout.
+    """
     def __init__(self, wrapped_class):
         self.wrapped_class = wrapped_class()
 
@@ -18,7 +28,6 @@ class Wrapper(object):
                 # prevent wrapped_class from becoming unwrapped
                 if result == self.wrapped_class:
                     return self
-                self.post()
                 return result
             return hooked
         else:
@@ -27,12 +36,13 @@ class Wrapper(object):
     def log(self, methname, *args, **kwargs):
         print "{}\n\tArgs: {}\n\tKwArgs: {}\n".format(methname, args, kwargs)
 
-    def post(self):
-        pass
-
 
 class RNRenderer(mistune.Renderer):
-    """Markdown to react-native renderer."""
+    """Markdown to react-native renderer.
+
+    Methods documentation taken from mistune default html renderer:
+    https://github.com/lepture/mistune/blob/master/mistune.py
+    """
 
     def header(self, text, level, raw=None):
         """Render header/heading tags like ``<h1>`` ``<h2>``.
