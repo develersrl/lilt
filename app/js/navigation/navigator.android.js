104273b3df76dc48960c5d975a65894e1b9237e0
Luca Benci <zero@develer.com>
2016-08-29 12:31:27 +0200

    Add experimental Android support

---
+++ app/js/navigation/navigator.android.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { Navigator, Text, TouchableHighlight } from 'react-native';
+
+import * as style from '../style';
+import { getStartRoute } from './navigator_data';
+
+export default class AppNavigator extends Component {
+  render() {
+    const { flexible } = style.common;
+    const { navigator } = style.pages;
+
+    return (
+      <Navigator
+        style={[flexible, navigator.nav]}
+        itemWrapperStyle={navigator.wrapped}
+        initialRoute={getStartRoute()}
+        navigationBar={
+          <Navigator.NavigationBar
+            style={navigator.nav}
+            routeMapper={navBarMapper}
+          />
+        }
+        renderScene={(route, navigator) => {
+          return <route.component navigator={navigator} {...route.passProps}/>
+        }}
+      />
+    );
+  }
+}
+
+const navBarMapper = {
+  LeftButton: (route, navigator, index, navState) => {
+    return (
+      <TouchableHighlight
+        onPress={() => {
+          if (index > 0) navigator.jumpBack()
+        }}>
+        <Text>Back</Text>
+      </TouchableHighlight>
+    )
+  },
+  RightButton: (route, navigator, index, navState) => {
+    return (
+      <TouchableHighlight
+        onPress={() => {
+          // if the current route is the last one, we can't jump forward
+          if (route !== navigator.getCurrentRoutes().slice(-1)[0]) {
+            navigator.jumpForward()
+          }
+        }}>
+        <Text>Forward</Text>
+      </TouchableHighlight>
+    )
+  },
+  Title: (route, navigator, index, navState) => {
+    return (<Text>{route.title}</Text>)
+  }
+}
