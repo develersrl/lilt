a2be0897e4f8a498d72a71ab89d8788a8f46d152
Alessandro Giannini <alessandro@develer.com>
2016-09-08 14:56:15 +0200

    Make eslint happy with the navigator code

--- app/js/navigation/navigator.js
+++ app/js/navigation/navigator.js
@@ -6,6 +6,40 @@ import { Navigator, Text, TouchableHighlight } from 'react-native';
 import * as style from '../style';
 import { getStartRoute } from './navigator_data';
 
+
+const navBarMapper = {
+  LeftButton: (route, navigator, index /* , navState */) => {
+    return (
+      <TouchableHighlight
+        onPress={() => {
+          if (index > 0)
+            navigator.jumpBack();
+        }}>
+        <Text>Back</Text>
+      </TouchableHighlight>
+    );
+  },
+  RightButton: (route, navigator /* , index, navState */) => {
+    return (
+      <TouchableHighlight
+        onPress={() => {
+          // if the current route is the last one, we can't jump forward
+          if (route !== navigator.getCurrentRoutes().slice(-1)[0]) {
+            navigator.jumpForward();
+          }
+        }}>
+        <Text>Forward</Text>
+      </TouchableHighlight>
+    );
+  },
+  Title: (route /* , navigator, index, navState */) => {
+    return (
+      <Text>{route.title}</Text>
+      );
+  }
+};
+
+
 export default class AppNavigator extends Component {
   render() {
     const { flexible } = style.common;
@@ -23,38 +57,11 @@ export default class AppNavigator extends Component {
           />
         }
         renderScene={(route, navigator) => {
-          return <route.component navigator={navigator} {...route.passProps}/>
+          return (
+            <route.component navigator={navigator} {...route.passProps}/>
+            );
         }}
       />
     );
-  }
-}
-
-const navBarMapper = {
-  LeftButton: (route, navigator, index, navState) => {
-    return (
-      <TouchableHighlight
-        onPress={() => {
-          if (index > 0) navigator.jumpBack()
-        }}>
-        <Text>Back</Text>
-      </TouchableHighlight>
-    )
-  },
-  RightButton: (route, navigator, index, navState) => {
-    return (
-      <TouchableHighlight
-        onPress={() => {
-          // if the current route is the last one, we can't jump forward
-          if (route !== navigator.getCurrentRoutes().slice(-1)[0]) {
-            navigator.jumpForward()
-          }
-        }}>
-        <Text>Forward</Text>
-      </TouchableHighlight>
-    )
-  },
-  Title: (route, navigator, index, navState) => {
-    return (<Text>{route.title}</Text>)
   }
 }
