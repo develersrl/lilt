dc159340e825c9d47c55e429632837dd03b26c79
Alessandro Giannini <alessandro@develer.com>
2016-09-15 18:27:34 +0200

    Generate link list page from json

--- app/content/templates/link_list.tmpl.js
+++ app/content/templates/link_list.tmpl.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import { View, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
-import { LinkListItem } from '../../blocks';
+import { LinkListItem, Button } from '../../blocks';
 import { common } from '../../style';
 
 
@@ -20,30 +20,7 @@ export default class LinkList extends Component {
         <ScrollView style={myStyle.contentView}
                     bounces={false}
                     showsVerticalScrollIndicator={false}>
-          <LinkListItem title={'Informazioni'}
-                        caption={loremIpsum}
-                        onLinkPress={() => navigator.push(getRoute('#9'))} />
-          <LinkListItem title={'Cause e Fattori'}
-                        caption={'Capitolo 1 / Capitolo 2'}
-                        onLinkPress={() => navigator.push(getRoute('#9'))} />
-          <LinkListItem title={'Terapia'}
-                        caption={'Capitolo 1 / Capitolo 2'}
-                        onLinkPress={() => navigator.push(getRoute('#9'))} />
-          <LinkListItem title={'Dopo la terapia'}
-                        caption={'Capitolo 1 / Capitolo 2 Capitolo 1 / Capitolo 2'}
-                        onLinkPress={() => navigator.push(getRoute('#9'))} />
-          <LinkListItem title={'Informazioni'}
-                        caption={loremIpsum}
-                        onLinkPress={() => navigator.push(getRoute('#9'))} />
-          <LinkListItem title={'Cause e Fattori'}
-                        caption={'Capitolo 1 / Capitolo 2'}
-                        onLinkPress={() => navigator.push(getRoute('#9'))} />
-          <LinkListItem title={'Terapia'}
-                        caption={'Capitolo 1 / Capitolo 2'}
-                        onLinkPress={() => navigator.push(getRoute('#9'))} />
-          <LinkListItem title={'Dopo la terapia'}
-                        caption={'Capitolo 1 / Capitolo 2 Capitolo 1 / Capitolo 2'}
-                        onLinkPress={() => navigator.push(getRoute('#9'))} />
+          {{entries}}
         </ScrollView>
       </View>
       );
