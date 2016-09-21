e945ade93e67de0d13da881ccf678e3644a0d283
Alessandro Giannini <alessandro@develer.com>
2016-09-21 12:00:32 +0200

    Use localhost as default js code location

--- app/ios/lilt/AppDelegate.m
+++ app/ios/lilt/AppDelegate.m
@@ -43,7 +43,8 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://10.3.16.144:8081/index.ios.bundle?platform=ios&dev=true"];
+  // jsCodeLocation = [NSURL URLWithString:@"http://10.3.16.144:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
