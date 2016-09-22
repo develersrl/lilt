3cc88837bad2f8a30dec5cc2d411086d3e9471d5
Alessandro Giannini <alessandro@develer.com>
2016-09-22 10:43:15 +0200

    Prepare generation scripts for glossary pages generation

--- app/scripts/common.py
+++ app/scripts/common.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import shutil
+import json
 
 
 # Base assumption: this script must be placed in the "scripts" directory
@@ -12,6 +14,15 @@ app_dir = os.path.dirname(scripts_dir)
 # The "content" directory contains input data for the generation script
 content_dir = os.path.join(app_dir, 'content')
 
+# Generation script takes pages data from this directory
+content_pages_dir = os.path.join(content_dir, 'pages')
+
+# Full path to json file containing pages description
+content_json_fn = os.path.join(content_dir, 'pages.json')
+
+# Pdf files used by app pages are located inside this folder
+pdf_dir = os.path.join(content_dir, 'pdf')
+
 # The "templates" directory contains react native component templates.
 # Templates will be instanced with Jinja during the generation phase.
 templates_dir = os.path.join(app_dir, 'templates')
@@ -21,3 +32,69 @@ target_pages_dir = os.path.join(app_dir, 'js', 'pages', 'generated')
 
 # App navigation code will be generated inside the following directory
 target_navigation_dir = os.path.join(app_dir, 'js', 'navigation')
+
+
+def listdir_nohidden(path):
+    result = []
+    for f in os.listdir(path):
+        if not f.startswith('.'):
+            yield f
+
+
+def clean_pdf_dir():
+    """Clean the pdf directory which is populated later."""
+    # Create the pdf directory if it does not exist
+    if not os.path.isdir(pdf_dir):
+        os.makedirs(pdf_dir)
+        return
+
+    # Get the pdf files list and remove them
+    pdf_files = [f for f in os.listdir(pdf_dir) if f.lower().endswith('pdf')]
+    for pdf_name in pdf_files:
+        os.remove(os.path.join(pdf_dir, pdf_name))
+
+
+def clean_target_pages_dir():
+    """Prepare target pages dir for the generation script."""
+    if not os.path.exists(target_pages_dir):
+        os.makedirs(target_pages_dir)
+    else:
+        shutil.rmtree(target_pages_dir)
+        os.mkdir(target_pages_dir)
+
+
+def get_content_page_dir(page_id):
+    """Return page data directory from page id."""
+    return os.path.join(content_pages_dir, page_id)
+
+
+def get_output_pdfname(pdf_name):
+    """Return bundled pdf filename, given a pdf_name."""
+
+    # Since we use the pdf name as output filename (e.g. "foo.pdf") there
+    # could be conflicts if two different generated pages have pdf files with
+    # the same name. To avoid the problem we add a suffix to the pdf name.
+    basename = os.path.splitext(pdf_name)[0]
+
+    same_pdf = 0
+    for f in os.listdir(pdf_dir):
+        if f.lower().startswith(basename):
+            same_pdf += 1
+
+    suffix = '' if same_pdf == 0 else '{}'.format(same_pdf)
+    return basename + suffix + '.pdf'
+
+
+def load_json(fn):
+    if not os.path.isfile(fn):
+        print '\t\tFAIL: cannot find page.json file'
+        return None
+
+    try:
+        with open(fn, 'r') as f:
+            json_content = json.load(f)
+    except ValueError:
+        print '\t\tFAIL: malformed json file'
+        return None
+
+    return json_content
--- app/scripts/editor_data_importer.py
+++ app/scripts/editor_data_importer.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import os
+import shutil
 
 import common
-from contents_importer import *
+from contents_importer import import_content_page
 
+
+# The following array describes the templates to be imported from editor dir
+editor_templates_descriptors = [
+    {
+        'dirname': 'Contenuti',
+        'genprefix': 'c_gen_',
+        'importfun': import_content_page
+    }
+]
 
 
 def get_editor_data_dir():
@@ -28,23 +38,57 @@ def get_editor_data_dir():
     return editor_data_dir
 
 
-def generate_content_pages(editor_data_dir):
-    """Generate app content ("Contenuti") pages."""
+def generate_page_data(page_id, input_page_dir, descriptor):
+    print '\tGenerating page "{}"'.format(page_id)
 
-    # Make sure that contents directory exists
-    contents_dir = os.path.join(editor_data_dir, 'Contenuti')
-    if not os.path.isdir(contents_dir):
-        print 'Warning: cannot find directory "Contenuti"'
+    # The editor saves a json file inside each page directory.
+    # Such file describe the various page tokens so we abort the generation
+    # if this file is not found.
+    input_json_fn = os.path.join(input_page_dir, 'page.json')
+    page_descriptor = common.load_json(input_json_fn)
+    if page_descriptor is None:
+        return None
+
+    # Remove and recreate output directory if exists
+    output_page_dir = common.get_content_page_dir(page_id)
+    if os.path.isdir(output_page_dir):
+        shutil.rmtree(output_page_dir)
+    os.mkdir(output_page_dir)
+
+    # Call custom import function to complete page generation
+    importfun = descriptor['importfun']
+    return importfun(page_id, page_descriptor, input_page_dir, output_page_dir)
+
+
+def generate_pages_data(editor_data_dir, descriptor):
+    # Compute editor template dir (e.g. "Contenuti", "Glossario") full path
+    editor_template_dir = os.path.join(editor_data_dir, descriptor['dirname'])
+    if not os.path.isdir(editor_template_dir):
+        print 'Warning: cannot find dir "{}"'.format(descriptor['dirname'])
         return []
 
-    return import_contents(contents_dir)
+    print 'Generating pages "{}"'.format(descriptor['dirname'])
+    generated_pages = []
+    dirs = common.listdir_nohidden(editor_template_dir)
+    for (i, dirname) in enumerate(dirs):
+        # We use a prefix for the page_id to prevent
+        # conflicts with other type of generated pages (e.g. glossary pages)
+        page_object = generate_page_data(
+            '{}{}'.format(descriptor['genprefix'], i + 1),
+            os.path.join(editor_template_dir, dirname),
+            descriptor)
+
+        # If generated page is None then something went wrong
+        if page_object is not None:
+            generated_pages.append(page_object)
+
+    return generated_pages
 
 
 def import_editor_data():
     """Read the editor output data and generate the corresponding app pages."""
 
     print '***** Generating app data from editor data *****'
-
     # If editor data directory is empty it means that something is wrong, so
     # we do not generate any page
     editor_data_dir = get_editor_data_dir()
@@ -54,8 +98,8 @@ def import_editor_data():
     # Generate pages from editor data
     print 'Editor data directory: {}'.format(editor_data_dir)
     generated_pages = []
-    generated_pages += generate_content_pages(editor_data_dir)
-
+    for descriptor in editor_templates_descriptors:
+        generated_pages += generate_pages_data(editor_data_dir, descriptor)
     print
 
     return generated_pages
