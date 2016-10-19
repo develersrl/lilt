6e49f3a0e30c4f538b1b10dd3cba2dee30cb5c7b
Alessandro Giannini <alessandro@develer.com>
2016-10-19 12:19:04 +0200

    Clean custom prevention page

--- app/js/pages/custom/custom_prevention.js
+++ app/js/pages/custom/custom_prevention.js
@@ -1,15 +1,40 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  ScrollView,
+} from 'react-native';
+
+import { common, pages } from '../../style';
 
 
-export default class CustomPrevention extends Component {
+export default class Content extends Component {
   render() {
+    const { flexible, centeredChildren } = common;
+    const { content } = pages;
+    const headerImage = require('../../../images/header_fallback.png');
+
     return (
-      <View>
-        <Text>Hello Custom Prevention!</Text>
-      </View>
+      <ScrollView style={[flexible]}
+                  bounces={false}
+                  showsVerticalScrollIndicator={false}
+                  automaticallyAdjustContentInsets={false}
+                  >
+        <Image style={content.header.container}
+               source={headerImage}>
+          <View style={[flexible, centeredChildren]}>
+            <Text style={[content.header.titleText, content.header.text]}>
+              Il mio percorso{"\n"}di prevenzione
+            </Text>
+          </View>
+        </Image>
+        <View style={[content.body.container]}>
+          <Text>Hello body!</Text>
+        </View>
+      </ScrollView>
       );
   }
 }
