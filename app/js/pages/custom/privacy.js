1ae9c3a2083f12e3e0592592a8079ceb16f53198
Gianni Valdambrini <aleister@develer.com>
2016-10-26 13:06:48 +0200

    Add privacy page content.

--- app/js/pages/custom/privacy.js
+++ app/js/pages/custom/privacy.js
@@ -6,20 +6,16 @@ import {
   StyleSheet,
   Image,
   Dimensions,
-  ScrollView,
   Text,
 } from 'react-native';
-
-import { common } from '../../style';
 
 
 export default class Privacy extends Component {
   render() {
-    const { navigator, getRoute } = this.props;
-    const { flexible } = common;
-
-    const loremIpsum = "Lorem Ipsum is simply dummy text of the printing";
+    /* eslint-disable max-len */
+    const text = "Preso atto dell’informativa di cui all’art. 13 del decreto legislativo 30 giugno 2003, n. 196, il Sottoscritto autorizza il trattamento e la comunicazione a LILT- Sezione di Firenze ONLUS (e a enti e/o consulenti che collaborano con LILT- Sezione di Firenze ONLUS, sempre nel rispetto delle finalità di detto Ente), autorizzando anche l’invio di materiale informativo e di altre informazioni relative alla prevenzione dei tumori. Autorizza inoltre l’uso dei dati per elaborazioni statistiche relative all’utilizzo della APP “LILT-FI seno”. Titolare del trattamento dei dati è LILT Lega Italiana per la Lotta contro i Tumori sezione di Firenze, Via D. Giannotti 23, 50126 Firenze";
     const title = "INFORMATIVA SULLA PRIVACY";
+    /* eslint-enable max-len */
 
     return (
       <View style={myStyle.container}>
@@ -28,6 +24,11 @@ export default class Privacy extends Component {
         <View style={myStyle.contentView}>
           <View style={myStyle.titleView}>
             <Text style={myStyle.titleText}>{title}</Text>
+          </View>
+          <View style={[myStyle.paragraphContainer]}>
+            <Text style={myStyle.paragraphText}>
+              {text}
+            </Text>
           </View>
         </View>
       </View>
@@ -64,5 +65,14 @@ const myStyle = StyleSheet.create({
     color: '#FF9C8D',
     fontFamily: 'GillSans-Bold',
     fontSize: 13,
+  },
+  paragraphContainer: {
+    padding: 10,
+  },
+  paragraphText: {
+    fontFamily: 'GillSans',
+    fontSize: 16,
+    color: '#8E8E8E',
+    lineHeight: 20,
   },
 });
