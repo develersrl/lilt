76ee3adfaaa0b7b9bec4013a09cfdab2e06f3169
Gianni Valdambrini <aleister@develer.com>
2016-10-27 12:09:57 +0200

    use a scrollable view also in the privacy as using a larger font the text can overflow the view
    Signed-off-by: Gianni Valdambrini <aleister@develer.com>

--- app/js/pages/custom/privacy.js
+++ app/js/pages/custom/privacy.js
@@ -4,34 +4,45 @@ import React, { Component } from 'react';
 import {
   View,
   StyleSheet,
+  ScrollView,
   Image,
   Dimensions,
   Text,
 } from 'react-native';
 
+import { common } from '../../style';
+
 
 export default class Privacy extends Component {
   render() {
+    const { flexible } = common;
+
     /* eslint-disable max-len */
     const text = "Preso atto dell’informativa di cui all’art. 13 del decreto legislativo 30 giugno 2003, n. 196, il Sottoscritto autorizza il trattamento e la comunicazione a LILT- Sezione di Firenze ONLUS (e a enti e/o consulenti che collaborano con LILT- Sezione di Firenze ONLUS, sempre nel rispetto delle finalità di detto Ente), autorizzando anche l’invio di materiale informativo e di altre informazioni relative alla prevenzione dei tumori. Autorizza inoltre l’uso dei dati per elaborazioni statistiche relative all’utilizzo della APP “LILT-FI seno”. Titolare del trattamento dei dati è LILT Lega Italiana per la Lotta contro i Tumori sezione di Firenze, Via D. Giannotti 23, 50126 Firenze";
     const title = "INFORMATIVA SULLA PRIVACY";
     /* eslint-enable max-len */
 
     return (
-      <View style={myStyle.container}>
-        <Image style={myStyle.backImg}
-               source={require('../../../images/back1.png')} />
-        <View style={myStyle.contentView}>
-          <View style={myStyle.titleView}>
-            <Text style={myStyle.titleText}>{title}</Text>
-          </View>
-          <View style={[myStyle.paragraphContainer]}>
-            <Text style={myStyle.paragraphText}>
-              {text}
-            </Text>
+      <ScrollView style={[flexible]}
+                  bounces={false}
+                  showsVerticalScrollIndicator={false}
+                  automaticallyAdjustContentInsets={false}
+                  >
+        <View style={myStyle.container}>
+          <Image style={myStyle.backImg}
+                 source={require('../../../images/back1.png')} />
+          <View style={myStyle.contentView}>
+            <View style={myStyle.titleView}>
+              <Text style={myStyle.titleText}>{title}</Text>
+            </View>
+            <View style={[myStyle.paragraphContainer]}>
+              <Text style={myStyle.paragraphText}>
+                {text}
+              </Text>
+            </View>
           </View>
         </View>
-      </View>
+      </ScrollView>
       );
   }
 }
